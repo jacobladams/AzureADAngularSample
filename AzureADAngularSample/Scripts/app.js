3e5e32492b8ed2c0168bf6828fdfd6da28e08ec6
jacobladams <jacobladams@gmail.com>
2016-02-02 11:21:07 -0600

    started adal.js library

--- AzureADAngularSample/Scripts/app.js
+++ AzureADAngularSample/Scripts/app.js
@@ -1,16 +1,29 @@
-﻿angular.module('AzureADAngularSample', []);
+﻿angular.module('AzureADAngularSample', ['AdalAngular'])
+.config(['$httpProvider', 'adalAuthenticationServiceProvider', function ($httpProvider, adalProvider) {
 
-angular.module('AzureADAngularSample').directive('test', function() {
+	adalProvider.init(adalConfigOptions, $httpProvider);
+
+}])
+.run(['adalAuthenticationService', function(adalAuthenticationService) {
+	adalAuthenticationService.login();
+}]);
+
+
+
+
+
+
+angular.module('AzureADAngularSample').directive('test', function () {
 	return {
 		restrict: 'E',
 		templateUrl: 'test',
-		controller: function($http) {
+		controller: function ($http) {
 			var vm = this;
 
 			vm.results = [];
 
-			vm.callService = function() {
-				$http.get(vm.url).then(function(results) {
+			vm.callService = function () {
+				$http.get(vm.url).then(function (results) {
 					vm.results = results.data;
 				});
 				//vm.results = ['hello'];
